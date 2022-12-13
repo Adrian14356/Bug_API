35d6bb13c36db98a806bf69203f70e1e73ce2a78
“github-example-dvm” <strzelczykadrian151@gmail.com>
2022-12-13 21:16:04 +0100

    serializer view commit

--- bug_api/desktop/views.py
+++ bug_api/desktop/views.py
@@ -12,32 +12,32 @@ class BugsListView(ListAPIView):
     model = Bug
     serializer_class = BugsSerializer
 
-    def get_queryset(self):
+    def get_queryset(self,request):
 
             project_id = self.request.GET.get("project")
             user_id = self.request.GET.get("user")
 
             if user_id and project_id is None:
-                messages.warning("404")
+                messages.warning(request, "404")
                 bugs = []
 
                 if user_id and project_id is not None:
                     messages.success(f"Project id {Bug.project}, user id {Bug.user}, bug {Bug.description}")
 
             elif project_id is None:
-                messages.warning("404")
+                messages.warning(request, "404")
                 bugs = Bug.object.filter(user__id=user_id).all()
 
                 if bugs is not None:
-                    messages.success(f"Project id {Bug.project}, bug {Bug.description}")
+                    messages.success(f"Project id {bugs.project}, bug {bugs.description}")
 
             elif user_id is None:
 
-                messages.warning("404")
+                messages.warning(request, "404")
                 bugs = Bug.object.filter(project__id=project_id).all()
 
                 if bugs is not None:
-                    messages.success(f"User id {Bug.user}, bug {Bug.description}")
+                    messages.success(f"User id {bugs.user}, bug {bugs.description}")
 
             else:
                 bugs = Bug.object.filter(project__id = project_id, user__id=user_id).all()
